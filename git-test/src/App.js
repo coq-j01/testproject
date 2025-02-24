f9714da6086da30f0298d987e636fec72f842b67
coq-j01 <chaebinesxo@gmail.com>
2025-02-24 16:50:38 +0900

    git test 이동

---
+++ git-test/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import KakaoMap from "./Kakao-map";
+
+function App() {
+  return (
+    <>
+      <KakaoMap />
+    </>
+  );
+}
+
+export default App;
